eac4e31e8150ea1f0da94d713fefd2983e425954
kryptos510 <jayvaja510@gmail.com>
2026-06-07 20:38:49 +0700

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9Ffix9HCkIogFzUOPdf0_PVLQC9ZCkAk",
+  authDomain: "jay-and-best.firebaseapp.com",
+  databaseURL: "https://jay-and-best-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jay-and-best",
+  storageBucket: "jay-and-best.firebasestorage.app",
+  messagingSenderId: "446479398081",
+  appId: "1:446479398081:web:2140341dd3f74d194d1040"
+};
+ 
+export default firebaseConfig;
